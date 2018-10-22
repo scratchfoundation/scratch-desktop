914f883c65f73b399e2e2d8f0397dac98887b2b6
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-10-22 12:36:01 -0700

    Eliminate redundant `index.js`->`index.jsx` shim

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,1 +1,23 @@
-import './index.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GUI, {AppStateHOC} from 'scratch-gui';
+import styles from 'scratch-gui/src/playground/index.css';
+
+// Register "base" page view
+// analytics.pageview('/');
+
+const appTarget = document.getElementById('app');
+appTarget.className = styles.app || 'app'; // TODO
+document.body.appendChild(appTarget);
+
+GUI.setAppElement(appTarget);
+const WrappedGui = AppStateHOC(GUI);
+
+if (process.env.NODE_ENV === 'production' && typeof window === 'object') {
+    // Warn before navigating away
+    window.onbeforeunload = () => true;
+}
+
+const wrappedGui = React.createElement(WrappedGui);
+ReactDOM.render(wrappedGui, appTarget);
