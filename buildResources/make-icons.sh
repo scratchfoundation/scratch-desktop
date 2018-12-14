ce1844ad0a9c19ee4c8def00a25660f8602eed17
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-12-13 18:20:39 -0800

    Fix Microsoft Store logo size

--- buildResources/make-icons.sh
+++ buildResources/make-icons.sh
@@ -55,8 +55,8 @@ if command -v convert >/dev/null 2>&1; then
     # Windows AppX
     mkdir -p "appx"
     resize 44 44 "${SRC}" 'appx/Square44x44Logo.png'
+    resize 50 50 "${SRC}" 'appx/StoreLogo.png'
     resize 150 150 "${SRC}" 'appx/Square150x150Logo.png'
-    resize 400 400 "${SRC}" 'appx/StoreLogo.png'
     resize 310 150 "${SRC}" 'appx/Wide310x150Logo.png'
 else
     echo "ImageMagick is not available - cannot convert icons"
