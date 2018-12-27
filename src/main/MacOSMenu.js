87d9a5e4488285a2493b66f50342c3fb782d7396
Paul Kaplan <pkaplan@media.mit.edu>
2018-12-27 13:49:26 -0500

    Include menu with default keyboard shortcuts to allow mac to work

---
+++ src/main/MacOSMenu.js
@@ -0,0 +1,50 @@
+// Include the standard keyboard shortcuts in the edit menu
+// so they can be used within the app. Only needed on Mac.
+export default app => ([
+    {
+        label: 'App', // Always overridden by app name
+        submenu: [{
+            label: 'Quit',
+            accelerator: 'CmdOrCtrl+Q',
+            click: () => app.quit()
+        }]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Undo',
+                accelerator: 'CmdOrCtrl+Z',
+                role: 'undo'
+            },
+            {
+                label: 'Redo',
+                accelerator: 'Shift+CmdOrCtrl+Z',
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Cut',
+                accelerator: 'CmdOrCtrl+X',
+                role: 'cut'
+            },
+            {
+                label: 'Copy',
+                accelerator: 'CmdOrCtrl+C',
+                role: 'copy'
+            },
+            {
+                label: 'Paste',
+                accelerator: 'CmdOrCtrl+V',
+                role: 'paste'
+            },
+            {
+                label: 'Select All',
+                accelerator: 'CmdOrCtrl+A',
+                role: 'selectall'
+            }
+        ]
+    }
+]);
