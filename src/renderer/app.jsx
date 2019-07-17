414a1f0cfde418855ed70d47d7dcb94168fbf818
Christopher Willis-Ford <cwillisf@media.mit.edu>
2019-07-17 16:56:56 -0700

    move index.js to app.jsx and add new index.js shim
    Note that the code inside app.jsx is exactly what used to be in index.js
    so this change causes no functional difference and just represents
    preparation for upcoming changes. It simplifies at least two upcoming
    changes: one wants the app code to be in a different file and the other
    is simplified by JSX syntax.

---
+++ src/renderer/app.jsx
@@ -0,0 +1,49 @@
+import {ipcRenderer, shell} from 'electron';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GUI, {AppStateHOC} from 'scratch-gui';
+
+import ElectronStorageHelper from '../common/ElectronStorageHelper';
+
+import styles from './app.css';
+
+const defaultProjectId = 0;
+
+// override window.open so that it uses the OS's default browser, not an electron browser
+window.open = function (url, target) {
+    if (target === '_blank') {
+        shell.openExternal(url);
+    }
+};
+// Register "base" page view
+// analytics.pageview('/');
+
+const appTarget = document.getElementById('app');
+appTarget.className = styles.app || 'app'; // TODO
+document.body.appendChild(appTarget);
+
+GUI.setAppElement(appTarget);
+const WrappedGui = AppStateHOC(GUI);
+
+const onStorageInit = storageInstance => {
+    storageInstance.addHelper(new ElectronStorageHelper(storageInstance));
+    // storageInstance.addOfficialScratchWebStores(); // TODO: do we want this?
+};
+
+const guiProps = {
+    onStorageInit,
+    isScratchDesktop: true,
+    projectId: defaultProjectId,
+    showTelemetryModal: (typeof ipcRenderer.sendSync('getTelemetryDidOptIn')) !== 'boolean',
+    onTelemetryModalOptIn: () => {
+        ipcRenderer.send('setTelemetryDidOptIn', true);
+    },
+    onTelemetryModalOptOut: () => {
+        ipcRenderer.send('setTelemetryDidOptIn', false);
+    },
+    onProjectTelemetryEvent: (event, metadata) => {
+        ipcRenderer.send(event, metadata);
+    }
+};
+const wrappedGui = React.createElement(WrappedGui, guiProps);
+ReactDOM.render(wrappedGui, appTarget);
