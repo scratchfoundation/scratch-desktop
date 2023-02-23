ac44ef17340d22bb15d8dc217f19fe62d0936eb7
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-02-23 16:22:28 +1300

    Update electron-builder-wrapper.js

--- scripts/electron-builder-wrapper.js
+++ scripts/electron-builder-wrapper.js
@@ -7,9 +7,6 @@
  */
 
 const {spawnSync} = require('child_process');
-const fs = require('fs');
-
-const masDevProfile = 'build/Development_edu.mit.scratch.scratch-desktop.provisionprofile';
 
 /**
  * Strip any code signing configuration (CSC) from a set of environment variables.
@@ -41,40 +38,21 @@ const getPlatformFlag = function () {
 
 /**
  * Run `electron-builder` once to build one or more target(s).
- * @param {object} wrapperConfig - overall configuration object for the wrapper script.
- * @param {object} target - the target to build in this call.
- * If the `target.name` is `'nsis'` then the environment must contain code-signing config (CSC_* or WIN_CSC_*).
- * If the `target.name` is `'appx'` then code-signing config will be stripped from the environment if present.
+ * @param {string} targetGroup - the target(s) to build in this pass.
+ * If the `targetGroup` is `'nsis'` then the environment must contain code-signing config (CSC_* or WIN_CSC_*).
+ * If the `targetGroup` is `'appx'` then code-signing config will be stripped from the environment if present.
  */
-const runBuilder = function (wrapperConfig, target) {
-    // the AppX build fails if CSC_* or WIN_CSC_* variables are set
-    const shouldStripCSC = (target.name.indexOf('appx') === 0) || (!wrapperConfig.doSign);
+const runBuilder = function (targetGroup) {
+    // the appx build fails if CSC_* or WIN_CSC_* variables are set
+    const shouldStripCSC = (targetGroup === 'appx');
     const childEnvironment = shouldStripCSC ? stripCSC(process.env) : process.env;
-    if (wrapperConfig.doSign &&
-        (target.name.indexOf('nsis') === 0) &&
-        !(childEnvironment.CSC_LINK || childEnvironment.WIN_CSC_LINK)) {
-        throw new Error(`Signing NSIS build requires CSC_LINK or WIN_CSC_LINK`);
+    if ((targetGroup === 'nsis') && !(childEnvironment.CSC_LINK || childEnvironment.WIN_CSC_LINK)) {
+        throw new Error(`NSIS build requires CSC_LINK or WIN_CSC_LINK`);
     }
     const platformFlag = getPlatformFlag();
-    let allArgs = [platformFlag, target.name];
-    if (target.platform === 'darwin') {
-        allArgs.push(`--c.mac.type=${wrapperConfig.mode === 'dist' ? 'distribution' : 'development'}`);
-        if (target.name === 'mas-dev') {
-            allArgs.push(`--c.mac.provisioningProfile=${masDevProfile}`);
-        }
-        if (wrapperConfig.doSign) {
-            // really this is "notarize only if we also sign"
-            allArgs.push('--c.afterSign=scripts/afterSign.js');
-        } else {
-            allArgs.push('--c.mac.identity=null');
-        }
-    }
-    if (!wrapperConfig.doPackage) {
-        allArgs.push('--dir', '--c.compression=store');
-    }
-    allArgs = allArgs.concat(wrapperConfig.builderArgs);
-    console.log(`running electron-builder with arguments: ${allArgs}`);
-    const result = spawnSync('electron-builder', allArgs, {
+    const command = `electron-builder ${platformFlag} ${targetGroup}`;
+    console.log(`running: ${command}`);
+    const result = spawnSync(command, {
         env: childEnvironment,
         shell: true,
         stdio: 'inherit'
@@ -91,114 +69,27 @@ const runBuilder = function (wrapperConfig, target) {
 };
 
 /**
- * @param {object} wrapperConfig - overall configuration object for the wrapper script.
- * @returns {Array.<object>} - the default list of targets on this platform. Each item in the array represents one
- * call to `runBuilder` for exactly one build target. In theory electron-builder can build two or more targets at the
- * same time but doing so limits has unwanted side effects on both macOS and Windows (see function body).
+ * @returns {Array.<string>} - the default list of target groups on this platform. Each item in the array represents
+ * one call to `runBuilder` for one or more build target(s).
  */
-const calculateTargets = function (wrapperConfig) {
-    const availableTargets = {
-        macAppStore: {
-            name: 'mas',
-            platform: 'darwin'
-        },
-        macAppStoreDev: {
-            name: 'mas-dev',
-            platform: 'darwin'
-        },
-        macDirectDownload: {
-            name: 'dmg',
-            platform: 'darwin'
-        },
-        microsoftStore: {
-            name: 'appx:ia32 appx:x64',
-            platform: 'win32'
-        },
-        windowsDirectDownload: {
-            name: 'nsis:ia32',
-            platform: 'win32'
-        }
-    };
-    const targets = [];
+const calculateTargets = function () {
     switch (process.platform) {
     case 'win32':
-        // Run in two passes so we can skip signing the AppX for distribution through the MS Store.
-        targets.push(availableTargets.microsoftStore);
-        targets.push(availableTargets.windowsDirectDownload);
-        break;
+        // run in two passes so we can skip signing the appx
+        return ['nsis', 'appx'];
     case 'darwin':
         // Running 'dmg' and 'mas' in the same pass causes electron-builder to skip signing the non-MAS app copy.
-        // Running them as separate passes means they can both get signed.
+        // Running them as separate passes means they both get signed.
         // Seems like a bug in electron-builder...
-        // Running the 'mas' build first means that its output is available while we wait for 'dmg' notarization.
-        // Add macAppStoreDev here to test a MAS-like build locally. You'll need a Mac Developer provisioning profile.
-        if (fs.existsSync(masDevProfile)) {
-            targets.push(availableTargets.macAppStoreDev);
-        } else {
-            console.log(`skipping target "${availableTargets.macAppStoreDev.name}": ${masDevProfile} missing`);
-        }
-        if (wrapperConfig.doSign) {
-            targets.push(availableTargets.macAppStore);
-        } else {
-            // electron-builder doesn't seem to support this configuration even if mac.type is "development"
-            console.log(`skipping target "${availableTargets.macAppStore.name}" because code-signing is disabled`);
-        }
-        targets.push(availableTargets.macDirectDownload);
-        break;
-    default:
-        throw new Error(`Could not determine targets for platform: ${process.platform}`);
+        return ['dmg', 'mas'];
+    case 'linux':
+        return ['AppImage', 'deb', 'pacman', 'snap', 'rpm'];
     }
-    return targets;
+    throw new Error(`Could not determine targets for platform: ${process.platform}`);
 };
 
-const parseArgs = function () {
-    const scriptArgs = process.argv.slice(2); // remove `node` and `this-script.js`
-    const builderArgs = [];
-    let mode = 'dev'; // default
-
-    for (const arg of scriptArgs) {
-        const modeSplit = arg.split(/--mode(\s+|=)/);
-        if (modeSplit.length === 3) {
-            mode = modeSplit[2];
-        } else {
-            builderArgs.push(arg);
-        }
-    }
-
-    let doPackage;
-    let doSign;
-
-    switch (mode) {
-    case 'dev':
-        doPackage = true;
-        doSign = false;
-        break;
-    case 'dir':
-        doPackage = false;
-        doSign = false;
-        break;
-    case 'dist':
-        doPackage = true;
-        doSign = true;
-    }
-
-    return {
-        builderArgs,
-        doPackage, // false = build to directory
-        doSign,
-        mode
-    };
-};
-
-const main = function () {
-    const wrapperConfig = parseArgs();
-
-    // TODO: allow user to specify targets? We could theoretically build NSIS on Mac, for example.
-    wrapperConfig.targets = calculateTargets(wrapperConfig);
-
-    for (const target of wrapperConfig.targets) {
-        runBuilder(wrapperConfig, target);
-    }
-};
-
-main();
+// TODO: allow user to specify targets? We could theoretically build NSIS on Mac, for example.
+const targets = calculateTargets();
+for (const targetGroup of targets) {
+    runBuilder(targetGroup);
+}
